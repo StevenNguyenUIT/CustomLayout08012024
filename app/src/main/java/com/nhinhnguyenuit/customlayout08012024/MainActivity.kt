4207919af0f03344d9a036c3a28f6c78c33bc1aa
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-06-24 06:12:26 -0500

    updated

--- app/src/main/java/com/nhinhnguyenuit/customlayout08012024/MainActivity.kt
+++ app/src/main/java/com/nhinhnguyenuit/customlayout08012024/MainActivity.kt
@@ -32,44 +32,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        editTextNum1 = findViewById(R.id.edtSo1)
-        editTextNum2 = findViewById(R.id.edtSo2)
-        btnName = findViewById(R.id.btnName)
-        txtNumber = findViewById(R.id.txtNum)
-        imageViewAvatar = findViewById(R.id.imgAvatar)
-        screen = findViewById(R.id.main)
-
-        val listAvatar = ArrayList<Int>()
-        listAvatar.add(R.drawable.avatar1)
-        listAvatar.add(R.drawable.avatar2)
-        listAvatar.add(R.drawable.avatar3)
-
-
-//        screen?.setBackgroundColor(Color.rgb(10,50,100))
-        btnName?.setOnClickListener {
-            val str1 = editTextNum1?.text.toString()
-            val str2 = editTextNum2?.text.toString()
-            val index = Random.nextInt(listAvatar.size)
-            imageViewAvatar?.setImageResource(R.drawable.avatar)
-            screen?.setBackgroundResource(listAvatar[index])
-            Toast.makeText(this, index.toString(), Toast.LENGTH_SHORT).show()
-            if (str1.isEmpty() || str2.isEmpty()) {
-                Toast.makeText(this, "Please input number", Toast.LENGTH_SHORT).show()
-            } else {
-                val min = str1.toInt()
-                val max = str2.toInt()
-
-                if (min < max) {
-                    txtNumber?.text = Random.nextInt(min, max).toString()
-                } else {
-                    Toast.makeText(this, "Min need lower Max", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-        }
     }
 
-    fun sayHi(view: View) {
-        Toast.makeText(this,"ImageButton Was Press", Toast.LENGTH_SHORT).show()
-    }
 }
